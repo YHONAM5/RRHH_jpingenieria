9aac638a3f3f53d1aa5f961985dad4db71930809
alex <arapaalex@gmail.com>
2023-11-23 11:35:26 -0500

    asda

--- app/Http/Controllers/preboleta/PreboletaController.php
+++ app/Http/Controllers/preboleta/PreboletaController.php
@@ -17,39 +17,40 @@ use Illuminate\Support\Facades\Mail;
 
 class PreboletaController extends Controller
 {
-    public function index($idPeriodo, $idContrato, $idDatoContable){
+    public function index($idPeriodo, $idContrato, $idDatoContable)
+    {
         $periodo = Periodo::find($idPeriodo);
-        $num_dias = $periodo->CantidadDeDias; 
+        $num_dias = $periodo->CantidadDeDias;
         $dia_inicio = $periodo->DiaDeInicioDelPeriodo;
         $dia_fin = $periodo->DiaDeFinDelPeriodo;
-        $empleado = Persona::join('empleado','persona.idPersona','=','empleado.idPersona')
-                                ->join('cargo','cargo.idCargo','=','empleado.idCargo')
-                                ->join('contrato','contrato.idEmpleado','=','empleado.idEmpleado')
-                                ->join('estaciondetrabajo','estaciondetrabajo.idEstacionDeTrabajo','=','contrato.idEstacionTrabajo')
-                                ->join('fondodepension','empleado.idFondoDePension','=','fondodepension.idFondoDePension')
-                                ->where('idContrato',$idContrato)
-                                
-                                ->first();
+        $empleado = Persona::join('empleado', 'persona.idPersona', '=', 'empleado.idPersona')
+            ->join('cargo', 'cargo.idCargo', '=', 'empleado.idCargo')
+            ->join('contrato', 'contrato.idEmpleado', '=', 'empleado.idEmpleado')
+            ->join('estaciondetrabajo', 'estaciondetrabajo.idEstacionDeTrabajo', '=', 'contrato.idEstacionTrabajo')
+            ->join('fondodepension', 'empleado.idFondoDePension', '=', 'fondodepension.idFondoDePension')
+            ->where('idContrato', $idContrato)
+
+            ->first();
         $dato_contable = Datoscontable::find($idDatoContable);
         $estaciones = Estaciondetrabajo::join('tareo', 'estaciondetrabajo.idEstacionDeTrabajo', '=', 'tareo.idEstacionDeTrabajo')
-                                        ->whereBetween('tareo.Fecha', [$dia_inicio, $dia_fin])
-                                        ->groupBy('estaciondetrabajo.idEstacionDeTrabajo', 'estaciondetrabajo.NombreEstacionDeTrabajo','estaciondetrabajo.estado','estaciondetrabajo.idRegimenLaboral')
-                                        ->select('estaciondetrabajo.*')
-                                        ->distinct()
-                                        ->get();
+            ->whereBetween('tareo.Fecha', [$dia_inicio, $dia_fin])
+            ->groupBy('estaciondetrabajo.idEstacionDeTrabajo', 'estaciondetrabajo.NombreEstacionDeTrabajo', 'estaciondetrabajo.estado', 'estaciondetrabajo.idRegimenLaboral')
+            ->select('estaciondetrabajo.*')
+            ->distinct()
+            ->get();
         $tareos = Tareo::join('contrato', 'tareo.idContrato', '=', 'contrato.idContrato')
-                                        ->join('empleado', 'contrato.idEmpleado', '=', 'empleado.idEmpleado')
-                                        ->join('persona', 'empleado.idPersona', '=', 'persona.idPersona')
-                                        ->join('datoscontables', 'contrato.idContrato', '=', 'datoscontables.idContrato')
-                                        ->join('estaciondetrabajo', 'contrato.idEstacionTrabajo', '=', 'estaciondetrabajo.idEstacionDeTrabajo')
-                                        ->join('fondodepension','empleado.idFondoDePension','=','fondodepension.idFondoDePension')
-                                        ->whereBetween('tareo.Fecha', [$dia_inicio, $dia_fin])
-                                        ->groupBy('contrato.idContrato', 'tareo.idDatoContable', 'persona.Nombres', 'persona.DNI', 'persona.ApellidoPaterno', 'estaciondetrabajo.NombreEstacionDeTrabajo', 'fondodepension.NombreEntidad', 'datoscontables.SueldoBase')
-                                        ->select('contrato.idContrato', 'tareo.idDatoContable', 'persona.Nombres', 'persona.DNI', 'persona.ApellidoPaterno', 'estaciondetrabajo.NombreEstacionDeTrabajo', 'fondodepension.NombreEntidad', DB::raw('MAX(datoscontables.SueldoBase) AS SueldoBase'))
-                                        ->get();
-        $num_estaciones =$estaciones->count();
+            ->join('empleado', 'contrato.idEmpleado', '=', 'empleado.idEmpleado')
+            ->join('persona', 'empleado.idPersona', '=', 'persona.idPersona')
+            ->join('datoscontables', 'contrato.idContrato', '=', 'datoscontables.idContrato')
+            ->join('estaciondetrabajo', 'contrato.idEstacionTrabajo', '=', 'estaciondetrabajo.idEstacionDeTrabajo')
+            ->join('fondodepension', 'empleado.idFondoDePension', '=', 'fondodepension.idFondoDePension')
+            ->whereBetween('tareo.Fecha', [$dia_inicio, $dia_fin])
+            ->groupBy('contrato.idContrato', 'tareo.idDatoContable', 'persona.Nombres', 'persona.DNI', 'persona.ApellidoPaterno', 'estaciondetrabajo.NombreEstacionDeTrabajo', 'fondodepension.NombreEntidad', 'datoscontables.SueldoBase')
+            ->select('contrato.idContrato', 'tareo.idDatoContable', 'persona.Nombres', 'persona.DNI', 'persona.ApellidoPaterno', 'estaciondetrabajo.NombreEstacionDeTrabajo', 'fondodepension.NombreEntidad', DB::raw('MAX(datoscontables.SueldoBase) AS SueldoBase'))
+            ->get();
+        $num_estaciones = $estaciones->count();
 
-        $pdf = Pdf::loadView('rrhh.preboleta.preboleta_pdf',compact('estaciones','tareos','num_estaciones','dia_inicio','dia_fin','num_dias','empleado','idContrato','idDatoContable','dato_contable','periodo'));
+        $pdf = Pdf::loadView('rrhh.preboleta.preboleta_pdf', compact('estaciones', 'tareos', 'num_estaciones', 'dia_inicio', 'dia_fin', 'num_dias', 'empleado', 'idContrato', 'idDatoContable', 'dato_contable', 'periodo'));
         return $pdf->stream();
     }
 
@@ -58,25 +59,22 @@ class PreboletaController extends Controller
         $idPeriodo = $request->input('idPeriodo');
         $destinatario = $request->input('destinatario');
         $url = $request->input('url');
-        $mensaje = $request->input('mensaje');  
+        $mensaje = $request->input('mensaje');
 
         $periodo = Periodo::find($idPeriodo);
         $nombre_periodo = $periodo->NombrePeriodo;
         $data = [
-            "subject"=>"ENVIO DE PREBOLETA",
-            "body"=>$mensaje,
-            "url"=> $url,
-            "nombre_periodo"=> $nombre_periodo
-            ];
-        try
-        {
-            $destinatarios = [$destinatario, 'recursoshumanos@awlmaquitec.com'];
+            "subject" => "ENVIO DE PREBOLETA",
+            "body" => $mensaje,
+            "url" => $url,
+            "nombre_periodo" => $nombre_periodo
+        ];
+        try {
+            $destinatarios = [$destinatario, 'jprrhh3@gmail.com'];
 
             Mail::to($destinatarios)->send(new Notification($data));
             return response()->json(['success' => true, 'message' => 'Envío de email exitoso']);
-        }
-        catch(Exception $e)
-        {
+        } catch (Exception $e) {
             return response()->json(['success' => false, 'error' => 'Hubo un problema al enviar la preboleta: ' . $e->getMessage()]);
         }
     }
